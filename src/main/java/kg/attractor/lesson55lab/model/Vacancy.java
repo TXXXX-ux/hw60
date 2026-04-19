3703d8628627a65a1731115bbb19a6e4c530fa65
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-19 21:07:59 +0600

    update Vacancy

--- src/main/java/kg/attractor/lesson55lab/model/Vacancy.java
+++ src/main/java/kg/attractor/lesson55lab/model/Vacancy.java
@@ -2,6 +2,7 @@ package kg.attractor.lesson55lab.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "vacancies")
@@ -12,7 +13,10 @@ public class Vacancy {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
     private Double salary;
+    private LocalDate publishedDate;
+    private Integer responsesCount;
 }
